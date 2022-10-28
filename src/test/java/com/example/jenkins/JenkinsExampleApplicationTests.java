c7d9cfe2bb245f2071e2d56230f597e233c5816e
vamsi355 <vamsi.krishna355@yahoo.com>
2022-10-28 20:54:51 +0530

    second commit

--- src/test/java/com/example/jenkins/JenkinsExampleApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsExampleApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.jenkins;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsExampleApplicationTests {
+	Logger  logger = LoggerFactory.getLogger(JenkinsExampleApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		
+		logger.info("Hello world , this is vamsi....!");
+		assertTrue(true);
 	}
 
 }
